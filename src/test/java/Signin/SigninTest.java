21d6ad3185aa89dca3b81294b2b9b351b640b8bf
Moh-AbuElfadl97 <143387321+Moh-AbuElfadl97@users.noreply.github.com>
2023-11-22 15:06:45 +0200

    Update SigninTest.java

--- src/test/java/Signin/SigninTest.java
+++ src/test/java/Signin/SigninTest.java
@@ -10,7 +10,7 @@ public class SigninTest extends BaseTest {
     @Test(priority = 1)
     public void testSuccessfullLogin(){
 
-        login("Abuelfadl_M909", "Abu716@203");
+        login("Username", "Password");
         TweetsPage timeLine = new TweetsPage(driver);
         Assert.assertTrue(timeLine.isHomeLinkVisible(),"Login was not successful. Home link is not visible.");
 
